85221066ae97b5149399e2770ffe3794f1e458d2
marcianoo21 <mgrzelak218@gmail.com>
2025-03-10 00:34:24 +0100

    structure expanded

---
+++ screens/onboarding/onboarding.screen.tsx
@@ -0,0 +1,26 @@
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { onBoardingSlides } from '@/configs/constants'
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import Slider from '@/components/onboarding/slider'
+import Slide from '@/components/onboarding/slide'
+
+export default function OnboardingScreen() {
+  const [index, setIndex] = useState(0)
+  const prev = onBoardingSlides[index - 1]
+  const next = onBoardingSlides[index + 1]
+
+  return (
+   <GestureHandlerRootView style={{flex: 1}}>
+      <Slider 
+      key={index}
+      index={index}
+      setIndex={setIndex}
+      prev={prev && <Slide slide={prev} totalSlides={onBoardingSlides.length} />}
+      next={next && <Slide  slide={next} totalSlides={onBoardingSlides.length} />}
+      >
+
+      </Slider>
+   </GestureHandlerRootView>
+  )
+}
