debc01afd166675daf3184b81fa1fcd4ebb77922
marcianoo21 <mgrzelak218@gmail.com>
2025-03-13 00:35:45 +0100

    s

--- components/onboarding/wave.tsx
+++ components/onboarding/wave.tsx
@@ -1,22 +1,33 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Animated ,{ SharedValue, useDerivedValue }  from 'react-native-reanimated'
+import Animated ,{ SharedValue, useDerivedValue, withSpring }  from 'react-native-reanimated'
 import SVG, { Path } from 'react-native-svg'
 import { Vector } from 'react-native-redash'
-import { Side } from '@/configs/constants'
+import { MIN_LEDGE, Side, WIDTH } from '@/configs/constants'
 
 const AnimatedPath = Animated.createAnimatedComponent(Path)
 
 interface WaveProps {
   side: Side,
   children: React.ReactElement,
-  position: Vector<SharedValue<number>>
+  position: Vector<SharedValue<number>>,
+  isTransitioning: SharedValue<boolean>
 }
 
 export default function Wave({side, children, position, isTransitioning}:WaveProps) {
   const R = useDerivedValue(() => {
-    const value = Math.min(position.x.value)
+    const value = Math.min(position.x.value - MIN_LEDGE, WIDTH / 2.5)
+    return value > 0 ? value : MIN_LEDGE
   })
+
+  const ledge = useDerivedValue(() => {
+    const baseLedge = Math.max(0, position.x.value - MIN_LEDGE - R.value)
+    return withSpring(isTransitioning.value ? position.x.value : baseLedge, {
+      stiffness: 200,
+    })
+  })
+
+  
   return (
     <View>
       <Text>Wave</Text>
