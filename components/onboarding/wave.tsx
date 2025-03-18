a4961b359b01453df66af080b4c1a82a6da78d8a
marcianoo21 <mgrzelak218@gmail.com>
2025-03-18 02:45:52 +0100

    slides animation

--- components/onboarding/wave.tsx
+++ components/onboarding/wave.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 import React from 'react'
-import Animated ,{ SharedValue, useDerivedValue, withSpring }  from 'react-native-reanimated'
-import SVG, { Path } from 'react-native-svg'
+import Animated ,{ SharedValue, useAnimatedProps, useAnimatedStyle, useDerivedValue, withSpring, withTiming }  from 'react-native-reanimated'
+import SVG, { Path, Svg } from 'react-native-svg'
 import { Vector } from 'react-native-redash'
-import { MIN_LEDGE, Side, WIDTH } from '@/configs/constants'
+import { HEIGHT, MIN_LEDGE, Side, WIDTH } from '@/configs/constants'
+import MaskedView from "@react-native-community/masked-view"
 
 const AnimatedPath = Animated.createAnimatedComponent(Path)
 
@@ -27,10 +28,91 @@ export default function Wave({side, children, position, isTransitioning}:WavePro
     })
   })
 
-  
-  return (
-    <View>
-      <Text>Wave</Text>
-    </View>
+
+  const animatedProps = useAnimatedProps(() => {
+    const stepY = position.x.value - MIN_LEDGE;
+    const stepX = R.value / 2;
+    const C = stepY * 0.5522847498;
+
+    // Safeguard each point calculation to avoid NaN
+    const p1x = ledge.value;
+    const p1y = position.y.value - 2 * stepY;
+
+    const p2x = p1x + stepX;
+    const p2y = p1y + stepY;
+
+    const p3x = p2x + stepX;
+    const p3y = p2y + stepY;
+
+    const p4x = p3x - stepX;
+    const p4y = p3y + stepY;
+
+    const p5x = p4x - stepX;
+    const p5y = p4y + stepY;
+
+    const d = [
+      "M 0 0",
+      `H ${p1x}`,
+      `V${p1y}`,
+      `C ${p1x} ${p1y + C} ${p2x} ${p2y} ${p2x} ${p2y}`,
+      `C ${p2x} ${p2y} ${p3x} ${p3y - C} ${p3x} ${p3y}`,
+      `C ${p3x} ${p3y + C} ${p4x} ${p4y} ${p4x} ${p4y}`,
+      `C ${p4x} ${p4y} ${p5x} ${p5y - C} ${p5x} ${p5y}`,
+      `V ${HEIGHT}`,
+      `H 0`,
+      "Z",
+    ];
+
+    return {
+      d: d.join(" "),
+    };
+  });
+
+  const maskElement = (
+    <Svg
+    style={[StyleSheet.absoluteFill, {
+      transform: [{rotateY: side === Side.RIGHT ? "180deg" : "0deg"}],
+    }]}
+    >
+      <AnimatedPath 
+       fill={
+        Platform.OS === "android"
+          ? children.props.slide.color
+          : children.props.color
+      }
+      animatedProps={animatedProps}/>
+    </Svg>
   )
+
+ const androidStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: isTransitioning.value
+            ? withTiming(0)
+            : side === Side.RIGHT
+            ? WIDTH - ledge.value
+            : -WIDTH + ledge.value,
+        },
+      ],
+    };
+  });
+
+  if (Platform.OS === "android") {
+    return (
+      <View style={StyleSheet.absoluteFill}>
+        {maskElement}
+        <Animated.View style={[StyleSheet.absoluteFill, androidStyle]}>
+          {children}
+        </Animated.View>
+      </View>
+    );
+  }
+
+  return (
+    // @ts-ignore
+    <MaskedView style={StyleSheet.absoluteFill} maskElement={maskElement}>
+      {children}
+    </MaskedView>
+  );
 }
